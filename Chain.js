3572f73d5eed976fe493dd56ecb23c028bdcbef9
Rejirey <rejireyfernandez@gmail.com>
2020-11-15 09:41:10 +0800

    class 28

---
+++ Chain.js
@@ -0,0 +1,20 @@
+class Sligshot {
+    constructor(bodyA,bodyB){
+        var options={
+            bodyA:bodyA,
+            bodyB:bodyB,
+            stiffness:0.04,
+            length:10
+        }
+        this.slingshot=Constraint.create(options);
+        World.add(world,this.slingshott);
+    }
+    
+    display(){
+        var pointA=this.chain.bodyA.position
+        var pointB=this.chain.bodyB.position        
+    strokeWeight(4);
+    line(pointA.x,pointA.y,pointB.x,pointB.y);
+    }
+
+}
--- sketch.js
+++ sketch.js
@@ -1,9 +1,10 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint= Matter.Constraint;
 
 var engine, world;
-var box1, pig1;
+var box1, pig1,log6, slingshot;
 var backgroundImg,platform;
 
 function preload() {
@@ -35,15 +36,14 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    log6= new Log(230,180,80,PI/2);
+    
+    slingshot = new Slingshot(bird.body,{x:200,y:200});
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
     box1.display();
     box2.display();
     ground.display();
@@ -60,5 +60,14 @@ function draw(){
     log5.display();
 
     bird.display();
-     platform.display();
+    platform.display();
+    log6.display();
+    slingshot.display();
+}
+function mouseDragged(){
+    Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY});
+}
+
+function mouseReleased(){
+    slingshot.fly();
 }
